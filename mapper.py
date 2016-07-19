a7a6f5e752c69eabd3f5ac7664fe870ee4ad83a4
Geoff Scott <geoff@hackerati.us>
2016-07-19 18:13:25 -0400

    implemented with iterators and genrators

--- mapper.py
+++ mapper.py
@@ -2,14 +2,22 @@
 
 import sys
 
-# input comes from STDIN (standard input)
-for line in sys.stdin:
-    line = line.strip () # remove leading and trailing whitespace
-    words = line.split () # split the line into words
+def read_input (file):
+    for line in file:
+        # split the line into words
+        yield line.split ()
 
-    # write the results to STDOUT (standard output);
-    # what we output here will be the input for the
-    # Reduce step, i.e. the input for reducer.py
-    #
-    # tab-delimited; the trivial word count is 1
-    [print (word + '\t' + '1') for word in words]
+def main (separator='\t'):
+    # input comes from STDIN (standard input)
+    lines = read_input (sys.stdin)
+
+    for line in lines:
+        # write the results to STDOUT (standard output);
+        # what we output here will be the input for the
+        # Reduce step, i.e. the input for reducer.py
+        #
+        # tab-delimited; the trivial word count is 1
+        [print (word + separator + '1') for word in line]
+
+if __name__ == "__main__":
+    main ()
