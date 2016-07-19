69eec6e2e3805e663821ff12961da44b37b960ee
Geoff Scott <geoff@hackerati.us>
2016-07-19 17:09:42 -0400

    strip non-alphanumeric characters

--- mapper.py
+++ mapper.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
 
 import sys
+import re
+
+pattern = re.compile ('[\W_]+')
 
 # input comes from STDIN (standard input)
 for line in sys.stdin:
-    line = line.strip () # remove leading and trailing whitespace
+    line = pattern.sub (' ', line)
+    # line = line.strip () # remove leading and trailing whitespace in the line
     words = line.split () # split the line into words
 
     # write the results to STDOUT (standard output);
